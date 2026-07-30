90da7de649a4a77da0fb994f74c61999fb3f4c98
Who5673 <windows7modrenedition@gmail.com>
2026-07-30 22:11:03 +0700

    commit

--- docs/frontend.ts
+++ docs/frontend.ts
@@ -1,2 +1,81 @@
 "use strict";
 
+const isMobile = window.matchMedia("screen and (max-width: 1023px)");
+const audioElement: HTMLAudioElement | null = document.querySelector("#trysong");
+const audio: HTMLAudioElement | null = document.querySelector("#trysound");
+const playBtn: HTMLImageElement | null = window.document.querySelector("#playbtn");
+const pauseBtn: HTMLImageElement | null = window.document.querySelector("#pausebtn");
+const repeatBtn: HTMLImageElement | null = window.document.querySelector("#repeatbtn");
+const norepeatBtn: HTMLImageElement | null = window.document.querySelector("#norepeatbtn");
+const dur = window.document.querySelector("#dur");
+const losslessOpt = document.querySelector("#lossless-opt");
+const flacOpt = document.querySelector("#flac-opt");
+
+const playsong = () => {
+    audioElement?.play();
+    playBtn?.classList.add("playing");
+    pauseBtn?.classList.add("playing");
+}
+const pausesong = () =>
+{
+    audioElement?.pause();
+    playBtn?.classList.remove("playing");
+    pauseBtn?.classList.remove("playing");
+}
+const repeatsong = () => {
+    if (audioElement !== null) audioElement.loop = true;
+    repeatBtn?.classList.add("on");
+    norepeatBtn?.classList.add("on");
+};
+const unrepeatsong = () =>
+{
+    if (audioElement !== null) audioElement.loop = false;
+    repeatBtn?.classList.remove("on");
+    norepeatBtn?.classList.remove("on");
+}
+const loadLossless = () => {
+    
+}
+if (
+    /* NOT A AND NOT B = NOT(A OR B)  */
+    !((audioElement === null || audioElement === undefined) || (playBtn === null || playBtn === undefined)) &&
+    (dur !== null && dur !== undefined)
+) {
+    /* Playpause listeners */
+    playBtn?.addEventListener("click", playsong);
+    pauseBtn?.addEventListener("click", pausesong);
+
+    /* audioElemnt listeners */
+    audioElement?.addEventListener("play", () => {
+        dur.textContent = `${audioElement?.currentTime}/${audioElement.duration}`;
+    })
+
+    audioElement?.addEventListener("ended", pausesong);
+
+    /* repetition listeners */
+    repeatBtn?.addEventListener("click", repeatsong);
+    norepeatBtn?.addEventListener("click", unrepeatsong);
+};
+
+
+losslessOpt?.addEventListener("click", loadLossless);
+
+if(isMobile.matches)
+{
+    let isShowed: boolean = false;
+
+    const dropdownBtn = document.querySelector(".dropdown-btn");
+    const dropdownResolution = document.querySelector(".dropdown-resolution");
+
+    dropdownBtn?.addEventListener("click", function(){
+        if (!isShowed) {
+            dropdownResolution?.classList?.add("show");
+            isShowed = true;
+            setTimeout(() => null, 200);
+        }
+        else {
+            dropdownResolution?.classList?.remove("show");
+            isShowed = false;
+        }
+    });
+}
